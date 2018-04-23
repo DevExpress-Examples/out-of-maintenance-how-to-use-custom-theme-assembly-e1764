b243d7d39fe4c18215d80dae8a9f7f5e863cd858
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/MyThemeAssembly/Class1.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyThemeAssembly {
+    public class MyThemeRegistrator {
+        public static void Register() {
+        }
+    }
+}
