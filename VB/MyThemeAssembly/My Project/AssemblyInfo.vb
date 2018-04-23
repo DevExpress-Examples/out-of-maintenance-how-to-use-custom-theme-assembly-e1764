0ae79834b8554022fb77da53f89fa98c538587a3
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/MyThemeAssembly/My Project/AssemblyInfo.vb
@@ -0,0 +1,52 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Reflection
+Imports System.Runtime.CompilerServices
+Imports System.Runtime.InteropServices
+Imports System
+Imports System.Security
+Imports System.Web.UI
+Imports DevExpress.Web.ASPxClasses
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+<Assembly: AssemblyTitle("MyThemeAssembly")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyConfiguration("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("MyThemeAssembly")>
+<Assembly: AssemblyCopyright("Copyright ©  2009")>
+<Assembly: AssemblyTrademark("")>
+<Assembly: AssemblyCulture("")>
+
+<Assembly: ThemesAssembly>
+<Assembly: WebResourceAssembly(104)>
+
+
+' Setting ComVisible to false makes the types in this assembly not visible 
+' to COM components.  If you need to access a type in this assembly from 
+' COM, set the ComVisible attribute to true on that type.
+<Assembly: ComVisible(False)>
+
+' The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("1a353b8f-191a-48ac-9cda-53528f759914")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' [assembly: AssemblyVersion("1.0.*")]
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
+
+<Assembly: WebResource("MyThemeAssembly.App_Themes.MyVioletTheme.ASPxNavBar.skin", "text")>
+<Assembly: WebResource("MyThemeAssembly.App_Themes.MyVioletTheme.styles.css", "text/css", PerformSubstitution := True)>
+<Assembly: WebResource("MyThemeAssembly.App_Themes.MyVioletTheme.nbCollapse.png", "image/png")>
+<Assembly: WebResource("MyThemeAssembly.App_Themes.MyVioletTheme.nbExpand.png", "image/png")>
+<Assembly: WebResource("MyThemeAssembly.App_Themes.MyVioletTheme.nbGroupHeaderBack.gif", "image/gif")>
+<Assembly: WebResource("MyThemeAssembly.App_Themes.MyVioletTheme.nbLoading.gif", "image/gif")>
